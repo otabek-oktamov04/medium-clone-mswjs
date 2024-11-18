904b90b375726440b76acab70fbbe48a9c1f6506
otabek-oktamov04 <oktamovotabek19@gmail.com>
2024-11-18 12:01:17 +0500

    added headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: "http://localhost:5173",
     trace: "on-first-retry",
+    headless: true,
   },
 
   /* Configure projects for major browsers */
