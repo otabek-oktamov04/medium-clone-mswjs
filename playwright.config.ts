5fba9044e87ab82255305e3c567a2c2a7423f310
otabek-oktamov04 <oktamovotabek19@gmail.com>
2024-11-21 14:19:32 +0500

    fixed

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,16 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["json", { outputFile: "reporter/report.json" }], ["html"]],
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    baseURL: "http://localhost:5173",
     trace: "on-first-retry",
-    headless: true,
   },
 
   /* Configure projects for major browsers */
--- tests/home/home.spec.tsx
+++ tests/home/home.spec.tsx
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test.describe("Home Page", () => {
   test.beforeEach(async ({ page }) => {
     // Navigate to home page before each test
-    await page.goto("/");
+    await page.goto("http://localhost:5173/");
   });
 
   test("should display all navigation buttons", async ({ page }) => {
